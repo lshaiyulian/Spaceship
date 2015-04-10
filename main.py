a65b09e0aead003700ef90d430cce03bd929a49f
Dielson Sales <dielsonscarvalho@gmail.com>
2015-04-10 03:00:58 -0300

    Using main function

--- main.py
+++ main.py
@@ -1,46 +1,57 @@
 __author__ = 'dielson'
 
-import pygame, sys
-from pygame.locals import *
+import pygame
+import sys
+from pygame.locals import * # all keys
+
 from spaceship import Spaceship
-
-pygame.init()
-
-windowSurface = pygame.display.set_mode((500, 400), 0, 32)
-pygame.display.set_caption('Hello world!')
-
-BLACK = (0, 0, 0)
-
-spaceship = Spaceship(windowSurface)
 
 keyleft_pressed = False
 keyright_pressed = False
 
-while True:
-    windowSurface.fill(BLACK)
-    spaceship.update_position()
-    pygame.display.flip()
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-        elif event.type == KEYDOWN:
-            if event.key == K_LEFT:
-                keyleft_pressed = True
-            elif event.key == K_RIGHT:
-                keyright_pressed = True
-        elif event.type == KEYUP:
-            if event.key == K_LEFT:
-                keyleft_pressed = False
-            elif event.key == K_RIGHT:
-                keyright_pressed = False
+BLACK = (0, 0, 0)
 
-    # Moves the spaceship
-    if keyright_pressed:
-        if spaceship.x_position < 430:
-            spaceship.go_right()
-    elif keyleft_pressed:
-        if spaceship.x_position > 0:
-            spaceship.go_left()
 
-    pygame.time.delay(15)
+def init_game():
+    global windowSurface, spaceship
+    pygame.init()
+    windowSurface = pygame.display.set_mode((500, 400), 0, 32)
+    pygame.display.set_caption('Hello world!')
+    spaceship = Spaceship(windowSurface)
+
+
+def main():
+    global event, keyleft_pressed, keyright_pressed
+    init_game()
+    while True:
+        windowSurface.fill(BLACK)
+        spaceship.update_position()
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == KEYDOWN:
+                if event.key == K_LEFT:
+                    keyleft_pressed = True
+                elif event.key == K_RIGHT:
+                    keyright_pressed = True
+            elif event.type == KEYUP:
+                if event.key == K_LEFT:
+                    keyleft_pressed = False
+                elif event.key == K_RIGHT:
+                    keyright_pressed = False
+
+        # Moves the spaceship
+        if keyright_pressed:
+            if spaceship.x_position < 430:
+                spaceship.go_right()
+        elif keyleft_pressed:
+            if spaceship.x_position > 0:
+                spaceship.go_left()
+
+        pygame.time.delay(10)
+
+
+if __name__ == "__main__":
+    main()
